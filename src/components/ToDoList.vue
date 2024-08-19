02f4c862b4602c53fd73119c3a4d0fdc9e0db595
QWE03\user <tomnt1775@gmail.com>
2024-08-19 19:46:53 +0900

    + AddTodo, change ToDoList

--- src/components/ToDoList.vue
+++ src/components/ToDoList.vue
@@ -1,60 +1,36 @@
 <template>
-    <div>
-        <h2>To-Do List</h2>
-        <input v-model="newTodo" placeholder="Add a new to-do" @keyup.enter="addTodo" />
-        <button @click="addTodo">Add</button>
-
-        <ul>
-            <li v-for="(todo, index) in todos" :key="index">
-                <input type="checkbox" v-model="todo.completed" />
-                <span :class="{ completed: todo.completed }">{{ todo.text }}</span>
-                <button @click="removeTodo(index)">Delete</button>
-            </li>
-        </ul>
-    </div>
+  <ul>
+    <li v-for="(todo, index) in todos" :key="index">
+      <input type="checkbox" v-model="todo.completed" @change="saveTodos" />
+      <span :class="{ completed: todo.completed }">{{ todo.text }}</span>
+      <button @click="removeTodo(index)">Delete</button>
+    </li>
+  </ul>
 </template>
 
 <script>
 export default {
-    name: 'ToDoList',
-    data() {
-        return {
-            newTodo: '',
-            todos: []
-        };
-    },
-
-    mounted() {
-        const savedTodos = localStorage.getItem('todos');
-        if (savedTodos) {
-            this.todos = JSON.parse(savedTodos);
-        }
-    },
-
-    methods: {
-        addTodo() {
-            if (this.newTodo.trim()) {
-                this.todos.push({ text: this.newTodo, completed: false });
-                this.newTodo = '';
-                this.saveTodos();
-            }
-        },
-
-        removeTodo(index) {
-            this.todos.splice(index, 1);
-            this.saveTodos();
-        },
-
-        saveTodos() {
-            localStorage.setItem('todos', JSON.stringify(this.todos));
-        }
+  name: 'ToDoList',
+  props: {
+    todos: {
+      type: Array,
+      required: true
     }
+  },
+  methods: {
+    removeTodo(index) {
+      this.$emit('remove-todo', index);
+    },
+    saveTodos() {
+      this.$emit('save-todos');
+    }
+  }
 };
 </script>
 
-    <style>
-        .completed {
-        text-decoration: line-through;
-        color: grey;
-        }
-    </style>
+<style>
+.completed {
+  text-decoration: line-through;
+  color: grey;
+}
+</style>
